de2712805b813003f3bab004c3220f14d7f77e1d
PickleNick2 <nicholas.hobden@gmail.com>
2025-09-17 17:15:57 +0200

    assignment-4-submission

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -14,6 +14,31 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+
+    struct thread_data *data = (struct thread_data*) thread_param;
+
+    // Wait
+    sleep(data->wait_to_obtain_ms/1000);
+
+    // Obtain mutex
+    int rc1 = pthread_mutex_lock(data->mutex);
+    if(rc1 != 0){
+        ERROR_LOG("Failed to lock mutex");
+        data->thread_complete_success = false;
+        return data;
+    }
+    //  Wait
+    sleep(data->wait_to_release_ms/1000);
+
+    // Release mutex
+    int rc2 = pthread_mutex_unlock(data->mutex);
+    if(rc2 != 0){
+        ERROR_LOG("Failed to unlock mutex");
+        data->thread_complete_success = false;
+        return data;
+    }
+
+    data->thread_complete_success = true;
     return thread_param;
 }
 
@@ -28,6 +53,31 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+
+
+    //Allocate memory for the thread_data struct
+    struct thread_data *data = malloc(sizeof(struct thread_data));
+    if(data == NULL){
+        ERROR_LOG("Could not allocate memory for thread_data");
+        return false;
+    }
+// Define all the parameters for the struct to be used in this function
+    data->mutex = mutex;
+    data->thread_complete_success = false;
+    data->wait_to_obtain_ms = wait_to_obtain_ms;
+    data->wait_to_release_ms = wait_to_release_ms;
+// Create the thread
+    int rc = pthread_create(thread, NULL, threadfunc, data);
+    if(rc != 0){
+        ERROR_LOG("pthread_create failed");
+        free(data);
+        return false;
+    }
+
+
+
+
+
+    return true;
 }
 
